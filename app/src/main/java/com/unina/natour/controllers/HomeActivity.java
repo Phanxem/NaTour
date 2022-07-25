36973a448f05cdbf72e6535963098fc99a93cd6a
Francesco <francesco.e.panico@gmail.com>
2022-07-25 17:44:55 +0200

    Add SplashScreenActivity

---
+++ app/src/main/java/com/unina/natour/controllers/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.unina.natour.controllers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.unina.natour.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
