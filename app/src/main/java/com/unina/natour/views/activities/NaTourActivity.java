3d70f1d4fb67c5c267aa20c3b5ba8eaaa7ad7426
Francesco <francesco.e.panico@gmail.com>
2022-09-13 17:40:07 +0200

    minor fix

--- app/src/main/java/com/unina/natour/views/activities/NaTourActivity.java
+++ app/src/main/java/com/unina/natour/views/activities/NaTourActivity.java
@@ -1,16 +1,55 @@
 package com.unina.natour.views.activities;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
 
+import com.unina.natour.models.NaTourModel;
 import com.unina.natour.views.observers.Observer;
 
-public class NaTourActivity extends FragmentActivity implements Observer {
+import java.util.ArrayList;
+import java.util.List;
+
+public class NaTourActivity extends AppCompatActivity implements Observer {
 
     public final String TAG = this.getClass().getSimpleName();
 
+    private List<NaTourModel> models;
+
     public NaTourActivity(){
+        this.models = new ArrayList<NaTourModel>();
     }
 
+    public List<NaTourModel> getModels() {
+        return models;
+    }
+
+    public void setModels(List<NaTourModel> models) {
+        this.models = models;
+    }
+
+    public void addModel(NaTourModel model){
+        this.models.add(model);
+    }
+
+    public void removeModel(NaTourModel model){
+        if(!models.isEmpty()) this.models.remove(model);
+    }
+
+    @Override
+    protected void onResume() {
+        for(NaTourModel model : models){
+            model.registerObserver(this);
+        }
+        super.onResume();
+    }
+
+    @Override
+    protected void onStop() {
+        for(NaTourModel model : models){
+            model.undergisterObserver(this);
+        }
+        super.onStop();
+    }
 
     @Override
     public void update() {
